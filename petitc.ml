d589633c336aac4d1266ac1064e41a056379f1ec
Hugo Peyraud-Magnin <hugo.pm@protonmail.com>
2022-11-16 15:37:14 +0100

    le main complet

--- petitc.ml
+++ petitc.ml
@@ -1,6 +1,59 @@
-let compile file =
-  try let lexbuf = Lexing.from_channel (open_in file) in while Lexer.token lexbuf <> Parser.EOF do (); done
-  with Lexer.Lexing_Error s -> Format.eprintf "Lexing error: %s\n" s
+
+(* Programme principal *)
+
+open Format
+open Lexing
+open Parser
+open Ast
+
+let usage = "usage: petitc [options] file.c"
+
+let parse_only = ref false
+
+let spec =
+  [
+    "--parse-only", Arg.Set parse_only, "  stop after parsing";
+  ]
+
+let file =
+  let file = ref None in
+  let set_file s =
+    if not (Filename.check_suffix s ".c") then
+      raise (Arg.Bad "no .c extension");
+    file := Some s
+  in
+  Arg.parse spec set_file usage;
+  match !file with Some f -> f | None -> Arg.usage spec usage; exit 1
+
+let report (b,e) =
+  let l = b.pos_lnum in
+  let fc = b.pos_cnum - b.pos_bol + 1 in
+  let lc = e.pos_cnum - b.pos_bol + 1 in
+  eprintf "File \"%s\", line %d, characters %d-%d:\n" file l fc lc
 
 let () =
-  Arg.parse [] compile ""
+  let c = open_in file in
+  let lb = Lexing.from_channel c in
+
+  let rec dumb_parser () =
+    match (Lexer.token lb) with
+    | Parser.EOF -> ()
+    | _ -> eprintf "token parse\n"; dumb_parser () in
+  try
+    dumb_parser ();
+    (* let f = Parser.file tok in  *)
+    (* Format.printf "%a@." pp_file f;  *)
+    if !parse_only then exit 0;
+    (*Interp.file f*)
+  with
+    | Lexer.Lexing_Error s ->
+	report (lexeme_start_p lb, lexeme_end_p lb);
+	eprintf "lexical error: %s@." s;
+	exit 1
+    | Parser.Error ->
+	report (lexeme_start_p lb, lexeme_end_p lb);
+	eprintf "syntax error@.";
+	exit 1
+    | e ->
+	eprintf "Anomaly: %s\n@." (Printexc.to_string e);
+	exit 2

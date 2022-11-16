fbf71507467a0dd19bfc7982d57380b916f7bfe9
faremy <faremy03@gmail.com>
2022-11-16 15:26:52 +0100

    petitc

--- petitc.ml
+++ petitc.ml
@@ -1,9 +1,6 @@
-open Ast
-
 let compile file =
   try let lexbuf = Lexing.from_channel (open_in file) in while Lexer.token lexbuf <> Parser.EOF do (); done
-  with Lexer.Lexing_Error (s, l) -> Format.eprintf "File \"%s\", line %d, characters %d-%d:\nLexing error: %s\n"
-    file l.line l.lcol l.rcol s
+  with Lexer.Lexing_Error s -> Format.eprintf "Lexing error: %s\n" s
 
 let () =
   Arg.parse [] compile ""

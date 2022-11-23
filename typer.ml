c97e4863333c92aeaab8f4f23feb5e4781c0b44e
hugopm <hugo.pm@protonmail.com>
2022-11-23 18:41:33 +0100

    typer rubriques

--- typer.ml
+++ typer.ml
@@ -12,18 +12,32 @@ let lvalue = function
 | _ -> false
 
 
-let type_expr var_env fct_env e = 
+let type_expr var_env fct_env typing = 
   let fail msg =
-    raise (Typing_Error (e.eloc, msg)) in
+    raise (Typing_Error (typing.eloc, msg)) in
   
-  match e.edesc with
-  | Const Null -> { t_edesc = T_Const Null; etyp = Void }
+  match typing.edesc with
+  (* Constantes *)
+  | Const Null -> { t_edesc = T_Const Null; etyp = Pointer Void }
   | Const (BoolCst b) -> { t_edesc = T_Const (BoolCst b); etyp = Bool }
   | Const (IntCst i) -> { t_edesc = T_Const (IntCst i); etyp = Int }
+
+  (* Variables *)
   | Ident s -> begin
-    try { t_edesc = T_Ident s; etyp = Smap.find s var_env }
-    with Not_found -> fail (Format.sprintf "variable %s is undefined" s)
-  end
+      try { t_edesc = T_Ident s; etyp = Smap.find s var_env }
+      with Not_found -> fail (Format.sprintf "variable '%s' is undeclared" s)
+    end
+
+  (* sizeof *)
   | Sizeof Void -> fail "void has no size"
   | Sizeof t -> { t_edesc = T_Sizeof t; etyp = Int}
+
+  (* Pointeur : amp et deref *)
+  (* Assignation lv = rv, ex. x = (y = 2) *)
+  (* Unop sur lvalue *)
+  (* Unop numérique *)
+  (* Binop numérique *)
+  (* Avancer/reculer un pointeur, + à symétriser *)
+  (* Distance entre deux pointeurs *)
+  (* Appel de fonction *)
   | _ -> fail "cas non gere"

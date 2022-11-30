ec3d4d59896eb38d142feb7a76aca0ffeb6c8c48
faremy <faremy03@gmail.com>
2022-11-30 16:28:39 +0100

    typage du while

--- typer.ml
+++ typer.ml
@@ -195,6 +195,12 @@ let rec type_stmt var_env fct_env expect in_loop typing =
         fail "continue outside of loop";
       T_Continue
 
-  | _ -> fail "adsasdoka"
+  | While (c_raw, s_raw) ->
+      let c_ty = type_expr var_env fct_env c_raw
+      and s_ty = type_stmt var_env fct_env expect true s_raw in
+
+      if equiv (c_ty.etyp, Void) then
+        fail "void value not ignored as it ought to be";
+      T_While (c_ty, s_ty)
 and type_block () = t_nothing
   

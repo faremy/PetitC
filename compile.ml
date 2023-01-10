9d338bac8d271f731f9b211225772269eea8dc3c
faremy <faremy03@gmail.com>
2023-01-10 20:41:56 +0100

    ajout des comparaisons et des return

--- compile.ml
+++ compile.ml
@@ -3,12 +3,14 @@ open Typed_ast
 open X86_64
 
 
+let fail () = failwith "impossible"
+
 let rec compile_lvalue expr =
   (* Pour une lvalue, renvoie l'adresse dans laquelle est stockée la valeur, au lieu de la valeur elle même *)
   match expr.t_edesc with
   | T_Ident { offset = o; v_depth = _ } -> movq !%rbp !%rax ++ addq (imm o) !%rax
   | T_Unop (Deref, e) -> compile_expr e
-  | _ -> failwith "impossible"
+  | _ -> fail ()
 and compile_expr expr =
   match expr.t_edesc with
   | T_Const (IntCst i) -> movq (imm i) !%rax
@@ -25,15 +27,17 @@ and compile_expr expr =
   | T_Unop (UMinus, e) -> compile_expr e ++ negq !%rax
   | T_Unop (Deref, e) -> compile_expr e ++ movq (ind rax) !%rax
   | T_Unop (Amp, e) -> compile_lvalue e
-  | T_Unop (Incr true as op, e) | T_Unop (Decr true as op, e) ->
+  | T_Unop (Incr true as op, e)
+  | T_Unop (Decr true as op, e) ->
     compile_lvalue e
-    ++ (match op with Incr _ -> incq | Decr _ -> decq | _ -> failwith "impossible") (ind rax)
+    ++ (match op with Incr _ -> incq | Decr _ -> decq | _ -> fail ()) (ind rax)
     ++ movq (ind rax) !%rax
-  | T_Unop (Incr false as op, e) | T_Unop (Decr false as op, e) ->
+  | T_Unop (Incr false as op, e)
+  | T_Unop (Decr false as op, e) ->
     compile_lvalue e
     ++ movq !%rax !%rbx
     ++ movq (ind rbx) !%rax
-    ++ (match op with Incr _ -> incq | Decr _ -> decq | _ -> failwith "impossible") (ind rbx)
+    ++ (match op with Incr _ -> incq | Decr _ -> decq | _ -> fail ()) (ind rbx)
 
   | T_Binop (Plus as op, e1, e2)
   | T_Binop (Minus as op, e1, e2)
@@ -42,7 +46,7 @@ and compile_expr expr =
     ++ pushq !%rax (* On sauvegarde le calcul du 2e terme *)
     ++ compile_expr e1
     ++ popq rbx
-    ++ (match op with Plus -> addq | Minus -> subq | Mul -> imulq | _ -> failwith "impossible") !%rbx !%rax
+    ++ (match op with Plus -> addq | Minus -> subq | Mul -> imulq | _ -> fail ()) !%rbx !%rax
   | T_Binop (Div as op, e1, e2)
   | T_Binop (Mod as op, e1, e2) ->
     compile_expr e2
@@ -51,7 +55,21 @@ and compile_expr expr =
     ++ popq rbx
     ++ movq (imm 0) !%rdx (* Il faut mettre à 0 les 64 derniers bits du dividende *)
     ++ idivq !%rbx
-    ++ (match op with Div -> nop | Mod -> movq !%rdx !%rax | _ -> failwith "impossible")
+    ++ (match op with Div -> nop | Mod -> movq !%rdx !%rax | _ -> fail ())
+  
+  | T_Binop (Eq as op, e1, e2)
+  | T_Binop (Neq as op, e1, e2)
+  | T_Binop (Lt as op, e1, e2)
+  | T_Binop (Le as op, e1, e2)
+  | T_Binop (Gt as op, e1, e2)
+  | T_Binop (Ge as op, e1, e2) ->
+    compile_expr e2
+    ++ pushq !%rax
+    ++ compile_expr e1
+    ++ popq rbx
+    ++ cmpq !%rbx !%rax
+    ++ (match op with Eq -> sete | Neq -> setne | Lt -> setl | Le -> setle | Gt -> setg | Ge -> setge | _ -> fail ()) !%al
+    ++ movzbq !%al rax
 
   | T_Assign (e1, e2) ->
     compile_expr e2
@@ -61,26 +79,34 @@ and compile_expr expr =
     ++ movq !%rbx (ind rax)
     ++ movq !%rbx !%rax (* a = b renvoie la valeur de a après assignation, c'est à dire la valeur de b *)
   | T_Sizeof _ -> movq (imm 8) !%rax
-  | _ -> failwith "aaa"
+  | _ -> fail ()
 
-let compile_var dv =
+let rec compile_stmt = function
+  | T_Expr e -> compile_expr e
+  | T_Block b -> compile_block b
+  | T_Return e -> (match e with None -> nop | Some v -> compile_expr v) ++ leave ++ ret
+  | _ -> fail ()
+
+and compile_var dv =
   (match dv.t_dv_init with
     | None -> nop
     | Some e -> compile_expr e)
   ++ movq !%rax (ind ~ofs:dv.t_dv_id.offset rbp)
-  
 
-let compile_stmt = function
-  | T_Expr e -> compile_expr e
-  | _ -> failwith "stmt pas gere"
-let compile_decl = function
-| T_Fct _ -> nop
-| T_Var dv -> compile_var dv
-| T_Stmt s -> compile_stmt s
+and compile_block b =
+  let compile_decl = function
+    | T_Fct _ -> nop
+    | T_Var dv -> compile_var dv
+    | T_Stmt s -> compile_stmt s
+  in List.fold_left (fun code d -> code ++ compile_decl d) nop b
+
 let real_fct f =
-  let code = ref (pushq !%rbp ++ movq !%rsp !%rbp ++ subq (imm f.t_df_frame_size) !%rsp) in
-  List.iter (fun d -> code := !code ++ compile_decl d) f.t_df_body;
-  !code ++ leave ++ ret
+  pushq !%rbp
+  ++ movq !%rsp !%rbp
+  ++ subq (imm f.t_df_frame_size) !%rsp
+  ++ compile_block f.t_df_body
+  ++ leave
+  ++ ret
    
 let stack_aligner ext_fct_name =
   pushq !%rbp ++
@@ -92,7 +118,6 @@ let stack_aligner ext_fct_name =
   popq rbp ++
   ret
 
-let putchar_wrapper = nop
 let compile_fct f = match f.t_df_id.name with
   | "f_1_malloc" -> stack_aligner "malloc"
   | "f_2_putchar" -> stack_aligner "putchar"
@@ -103,7 +128,8 @@ let compile_prog prog funs main_id =
   List.iter (fun f -> tx := !tx ++ label f.t_df_id.name ++ compile_fct f) funs;
   tx := !tx ++ label "main" ++ call main_id ++ movq (imm 0) !%rax ++ ret;
   let p =
-    { text = !tx;
+    {
+      text = !tx;
       data = nop
     }
   in
